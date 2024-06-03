76473f00ef72cce0185afa9f86d665c04af9c2a2
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-06-03 16:10:19 +0530

    proof of concent test

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+let ETH_BALANCE =  200;
+let USDC_BALANCE = 700000;
+
+// app.post("/add-liquidity", (req, res) => {
+
+// })
+
+app.post("/buy-asset", (req, res) => {
+
+    const quantity = req.body.quantity;
+    const updatedEthQuantity = ETH_BALANCE - quantity;
+    const updatedUsdcBalance = ETH_BALANCE * USDC_BALANCE / updatedEthQuantity
+    const paidAmount = updatedUsdcBalance - USDC_BALANCE;
+    ETH_BALANCE = updatedEthQuantity;
+    USDC_BALANCE = updatedUsdcBalance;
+
+    res.json({
+        message: `Yout paid ${paidAmount} USDC for ${quantity} ETH`
+    });
+
+})
+
+app.post("/sell-asset", (req, res) => {
+
+    const quantity = req.body.quantity;
+    const updatedUsdcBalance = USDC_BALANCE - quantity;
+    const updatedEthBalance = ETH_BALANCE * USDC_BALANCE / updatedUsdcBalance;
+    const paidAmount = updatedEthBalance - ETH_BALANCE;
+    ETH_BALANCE = updatedEthBalance;
+    USDC_BALANCE = updatedUsdcBalance;
+
+    res.json({
+        message: `Yout paid ${paidAmount} ETH for ${quantity} USDC`
+    });
+
+})
+
+app.post("/quote", (req, res) => {
+
+})
