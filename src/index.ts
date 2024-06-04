92ee9fd73daa3d9d1a1f484c744366d2cdb53640
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-06-04 13:25:24 +0530

    mvp of selling, buying ETH, USDC done

--- src/index.ts
+++ src/index.ts
@@ -6,68 +6,110 @@ app.use(express.json());
 let ETH_BALANCE =  200;
 let USDC_BALANCE = 700000;
 
-app.post("/add-liquidity", (req, res) => {
-    const {eth, Usdc} = req.body.liquidity;
-    const updatedEthBalance = ETH_BALANCE + eth;
-    const updatedUsdcBalance = USDC_BALANCE + Usdc;
+// app.post("/add-liquidity", (req, res) => {
+//     const {eth, usdc} = req.body.liquidity;
+//     const ethReserve = ETH_BALANCE;
+//     const usdcReserve = USDC_BALANCE;
 
-    ETH_BALANCE = updatedEthBalance;
-    USDC_BALANCE = updatedUsdcBalance;
+//     if (ethReserve === 0 && usdcReserve === 0) {
+//         ETH_BALANCE = eth;
+//         USDC_BALANCE = usdc;
+//     } else {
+//         const ethRatio = eth / ethReserve;
+//         const usdcRatio = usdc / usdcReserve;
+
+//         if (ethRatio !== usdcRatio) {
+//             return res.status(400).json({
+//                 error: `Incorrect liquidity ratio. Please provide equal ratio of ETH and USDC`
+//             });
+//         }
+//         ETH_BALANCE += eth;
+//         USDC_BALANCE += usdc;
+//     }
+//     
+//     res.json({
+//         message: `Updated ETH: ${ETH_BALANCE} and USDC:  ${USDC_BALANCE}`
+//     });
+// })
+
+app.post("/add-liquidity", (req, res) => {
+    const { eth, usdc } = req.body.liquidity;
+    const ethReserve = ETH_BALANCE;
+    const usdcReserve = USDC_BALANCE;
+
+    if (ethReserve === 0 && usdcReserve === 0) {
+        ETH_BALANCE = eth;
+        USDC_BALANCE = usdc;
+    } else {
+        const ethRatio = eth / ethReserve;
+        const usdcRatio = usdc / usdcReserve;
+
+        if (ethRatio !== usdcRatio) {
+            return res.status(400).json({
+                error: "Incorrect liquidity ratio. Please provide equal ratio of ETH and USDC."
+            });
+        }
+
+        ETH_BALANCE += eth;
+        USDC_BALANCE += usdc;
+    }
 
     res.json({
-        message: `Updated ETH : ${ETH_BALANCE} and updated USDC: ${USDC_BALANCE}`
+        message: `Updated ETH: ${ETH_BALANCE} and USDC: ${USDC_BALANCE}`
     });
-})
+});
 
 app.post("/buy-asset", (req, res) => {
-    const quantity = req.body.quantity; //1
-    const updatedEthQuantity = ETH_BALANCE - quantity; // 199
-    const updatedUsdcBalance = ETH_BALANCE * USDC_BALANCE / updatedEthQuantity; // 200 * 700000 / 199
-    const paidAmount = updatedUsdcBalance - USDC_BALANCE; // 703517.587939698 - 700000 = 3517.587939698
 
-    ETH_BALANCE = updatedEthQuantity; // 199
-    USDC_BALANCE = updatedUsdcBalance; // 703517.587939698
+    const {quantity} = req.body;
+    const ethReserve = ETH_BALANCE;
+    const usdcReserve = USDC_BALANCE;
+
+    const paidAmount = (usdcReserve * quantity) / (ethReserve - quantity);
+    const newEthReserve = ethReserve - quantity;
+    const newUsdcReserve = usdcReserve + paidAmount;
+
+    ETH_BALANCE = newEthReserve;
+    USDC_BALANCE = newUsdcReserve;
 
     res.json({
-        message: `Yout paid ${paidAmount} USDC for ${quantity} ETH`
+        message: `You paid ${paidAmount} USDC for ${quantity} ETH`
     });
-
-})
+});
 
 app.post("/sell-asset", (req, res) => {
-    const quantity = req.body.quantity; // 1
-    const updatedEthBalance = ETH_BALANCE + quantity; // 200
-    const updatedUsdcBalance = ETH_BALANCE * USDC_BALANCE / updatedEthBalance; // 199 * 703517.587939698 / 200
-    const gottenUsdc = USDC_BALANCE - updatedEthBalance; // 703517.587939698 - 700000 = 3517.587939698
+    const {quantity} = req.body;
+    const ethReserve = ETH_BALANCE;
+    const usdcReserve = USDC_BALANCE;
+    
+    const gottenUsdc = (usdcReserve * quantity) / (ethReserve + quantity);
+    const newEthReserve = ethReserve + quantity;
+    const newUsdcReserve = usdcReserve - gottenUsdc; 
 
-    ETH_BALANCE = updatedEthBalance; // 200
-    USDC_BALANCE = updatedUsdcBalance; // 700000 
+    ETH_BALANCE = newEthReserve;
+    USDC_BALANCE = newUsdcReserve;
 
     res.json({
-        message: `Yout got ${gottenUsdc} USDC for ${quantity} ETH`
+        message: `You received ${gottenUsdc} USDC for ${quantity} ETH`
     });
-
 })
 
 app.post("/quote", (req, res) => {
     const {side, quantity} = req.body;
-    const ethToUsdcRatio = USDC_BALANCE / ETH_BALANCE;
+    const ethReserve = ETH_BALANCE;
+    const usdcReserve = USDC_BALANCE;
 
     if (side === "buy") {
-        const usdcRequired = quantity * ethToUsdcRatio;
+        const usdcRequired = (usdcReserve * quantity) / (ethReserve - quantity);
         res.json({
             quote: usdcRequired,
             message: `To buy ${quantity} ETH, you need ${usdcRequired} USDC`
         });
     } else if (side === "sell") {
-        const usdcToReceive = quantity * ethToUsdcRatio;
+        const usdcToReceive = (usdcReserve * quantity) / (ethReserve + quantity);
         res.json({
             quote: usdcToReceive,
-            message: `For sellign ${quantity} ETH, you will receive ${usdcToReceive}`
-        });
-    } else {
-        res.status(400).json({
-            error: "Invalid side. Please provide 'buy' or 'sell'."
+            message: `For selling ${quantity} ETH, you will receive ${usdcToReceive}`
         });
     }
 });
