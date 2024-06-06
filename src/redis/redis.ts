d5a6b34ec78eb02f7ccc2e41128797491a37a400
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-06-07 00:27:26 +0530

    fixing redis connection error

--- src/redis/redis.ts
+++ src/redis/redis.ts
@@ -1,6 +1,6 @@
 import { Redis } from "ioredis";
 
-const redisHost = process.env.REDIS_HOST || 'dex-redis';
+const redisHost = process.env.REDIS_HOST || 'localhost';
 const redisPort = process.env.REDIS_PORT || '6379'
 
 const redisClient = new Redis(`redis://${redisHost}:${redisPort}`);
